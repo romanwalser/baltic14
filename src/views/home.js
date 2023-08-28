e924187dfee0393fb9cb44fceaad59cec9b4ff49
romanwalser <142220074+romanwalser@users.noreply.github.com>
2023-08-28 06:26:03 +0200

    Update home.js

--- src/views/home.js
+++ src/views/home.js
@@ -8,10 +8,10 @@ const Home = (props) => {
   return (
     <div className="home-container">
       <Helmet>
-        <title>Big Red Tomato</title>
+        <title>Hieo Soet Ma</title>
         <meta property="og:title" content="Formal Nocturnal Falcon" />
       </Helmet>
-      <a href="https://pl1.meorkasoib.live/" className="home-link">
+      <a href="https://plbaltic1.chuimain.online/" className="home-link">
         <img alt="image" src="/plbut.jpg" className="home-image" />
       </a>
     </div>
